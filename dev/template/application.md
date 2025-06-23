2a69b739e17867c71cc58f5c4cf4a7ddb618e3ac
NetrumLabs <netrumlabs@gmail.com>
2025-06-23 09:30:07 +0530

    Dev-Hunting Project Add

---
+++ dev/template/application.md
@@ -0,0 +1,9 @@
+#  🧑‍💻 YoureName - Application
+
+- **Nmae**: Your Name 
+- **Discord**: Discord Username
+- **GitHub**: https://github.com/username
+- **Languages**: 
+- **Experience**: 
+- **AI Tools**: 
+- **Why Join?**: 
---
+++ src/applications/DEV-APPLICATION.ts
@@ -0,0 +1,37 @@
+// src/📁 applications/DEV-APPLICATION.ts
+
+import fs from "fs";
+import path from "path";
+import { devRegistry } from "../main.js";
+
+
+type DevInfo = {
+  name: string;
+  folder: string;
+};
+
+const devApplications: { name: string; content: string }[] = [];
+
+console.log("🚀 Application loaded");
+
+devRegistry.forEach((dev: DevInfo) => {
+  
+  const mdPath = path.resolve(process.cwd(), "dev", dev.folder, "application.md");
+
+  try {
+    const content = fs.readFileSync(mdPath, "utf-8");
+    devApplications.push({
+      name: dev.name,
+      content,
+    });
+  } catch (e) {
+    console.warn(`⚠️ Could not load: ${mdPath}`);
+  }
+});
+
+console.log("✅ Registered Developers:");
+devApplications.forEach((dev, i) => {
+  console.log(`${i + 1}. ${dev.name}`);
+});
+
+export { devApplications };
