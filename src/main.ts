9239a93962d9f8bfc791a913a184dd74e19028cd
Netrum Labs <netrumlabs@gmail.com>
2025-07-30 23:23:38 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 // src/main.ts
-
 export const devRegistry = [
   {
-    name: "Saandy",
-    folder: "Saandy-dev",
+    name: "Your Name",
+    folder: "template",
   },
   
   // 🔰 New devs just add entry here
